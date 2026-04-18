e715cfb1c9d92d32ccd39665e72abf058e4aabd7
BISWAJIT SINHA <biswajit.sinha.dev@gmail.com>
2026-04-18 19:57:13 +0530

    Remove .env from repo

---
+++ lib/Screen/test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class Test extends StatefulWidget {
+  const Test({super.key});
+  @override
+  State<Test> createState() => _TestState();
+}
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Container(
+        child: TextField(
+          decoration: InputDecoration(
+          ),
+        ),
+      ),
+    );
+  }
+}
+
